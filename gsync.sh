6467c04fabfacb9a1d3f96cbdaad2cf20f72cb64
mwelsch <welschmorgan@gmail.com>
2014-02-15 00:09:06 +0100

    Synchronizing Project

--- gsync.sh
+++ gsync.sh
@@ -2,13 +2,15 @@
 
 function sync_folder()
 {
+	make fclean
 	git add .
 	git commit -am "Synchronizing Project"
 	git push
 }
 
-sync_folder
 cd libft
 sync_folder
 cd ../libmath
+sync_folder
+cd ..
 sync_folder
