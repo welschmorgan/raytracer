f400213702d20f7a6b0066a9d286cb24111e0c50
mwelsch <welschmorgan@gmail.com>
2014-02-16 12:20:42 +0100

    Synchronizing Project

--- gclean.sh
+++ gclean.sh
@@ -12,4 +12,5 @@ for mod in libft libmath; do
 done
 myrm .gitmodules
 myrm *.sh
+myrm libft/src/ft_search_file.c libft/src/ft_file.c
 git remote set-url origin vogsphere@vogsphere.42.fr:infog-1-001/2013/par-1-1/rtv1/mwelsch
